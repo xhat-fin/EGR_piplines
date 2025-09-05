0a4d907dc9d2edece47dcc5b59544d388c0d7b65
nkhatkevich <openai@gmail.com>
2025-09-05 21:33:15 +0300

    get data

---
+++ api.py
@@ -0,0 +1,9 @@
+# import requests as rq
+#
+# response = rq.get('https://api.nbrb.by/exrates/rates/dynamics/431?startDate=2025-01-01&endDate=2025-09-04')
+# data = response.json()
+# print(data)
+#
+#
+# with open('data.json', 'w') as file:
+#     file.write(str(data))
