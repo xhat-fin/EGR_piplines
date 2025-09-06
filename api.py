cc9cf0c9c9bfdf3cb9abc502b710082a6b635534
nkhatkevich <openai@gmail.com>
2025-09-06 21:09:24 +0300

    lesson progress for pandas

--- api.py
+++ api.py
@@ -3,8 +3,6 @@ import json
 
 response = requests.get('https://api.nbrb.by/exrates/rates/dynamics/431?startDate=2024-09-06&endDate=2025-09-05')
 data = response.json()
-print(data)
 
-# Сохранение в файл с правильным форматированием
 with open('data.json', 'w', encoding='utf-8') as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
